4829048627ab26239e54893fd68791ac39e334f8
Antonio King <antonio.j.king@gmail.com>
2021-04-23 09:44:11 -0600

    Add model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+SCHOOL = {
+  name_of_school: "Fall River Elementary",
+  name_of_principal: "Mr. O'Keefe",
+  number_of_students: 450,
+  star_rating: 6.5,
+  has_special_ed_teacher: true,
+  is_closed_campus: true,
+  grade_levels: ["Pre-K", "K", "First", "Second", "Third", "Fourth", "Fifth"],
+  kindergarden_teachers: ["Ms. Sampish", "Mrs. Ekern", "Mrs. Jones"]
+}
