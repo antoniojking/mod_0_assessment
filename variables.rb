1f0b32404951b89d26c92c924a2ccc3744299b65
Antonio King <antonio.j.king@gmail.com>
2021-04-23 09:26:37 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,14 @@
+famous_person = "Julia Roberts"
+is_active = true
+
+famous_person = "Brad Pitt"
+num_3 = 3
+num_9 = 9
+
+sum = num_3 + num_9
+
+animals = ["tiger", "lion", "alligator"]
+
+animals.delete_at(0)
+
+p famous_person.length
