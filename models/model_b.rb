6db24256de046db69e01afa29218d99fb700927d
Antonio King <antonio.j.king@gmail.com>
2021-04-23 09:55:10 -0600

    Add model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+PET = {
+  type_of_pet: "dog",
+  name_of_pet: "Mathias",
+  age_of_pet_months: 8,
+  weight_of_pet_lbs: 40.6,
+  is_pet_vaccinated: true,
+  is_pet_fixed: false,
+  hobbies: ["napping", "fetching", "swimming", "cuddling"],
+  commands: ["sit", "stay", "lie down", "go home"]
+}
